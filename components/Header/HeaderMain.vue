2cffea15925c7094b503143d1a9904fd11bd80ad
Santhoshkumar Kothandaraman <santhosh@tazapay.com>
2021-07-12 22:35:09 +0530

    reload

--- components/Header/HeaderMain.vue
+++ components/Header/HeaderMain.vue
@@ -1,6 +1,14 @@
 <template>
     <div class=" bg-red-400 p-5 flex items-center justify-between">
         <div class="font-bold text-white text-5xl filter drop-shadow-xl stroke-current">Bulls Eye</div>
-        <div class="font-bold text-white text-2xl filter drop-shadow-xl stroke-current float-right  ">Performance</div>
+        <div class="font-bold text-white text-2xl filter drop-shadow-xl stroke-current float-right cursor-pointer" v-on:click="reload">Performance</div>
     </div>
 </template>
+
+<script>
+export default {
+methods:{
+reload(){
+    window.location.reload()
+}}}
+</script>
